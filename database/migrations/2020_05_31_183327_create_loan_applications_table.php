a0220beb2b72abdc3b57ccf426f069ed74206418
isaac <isaac.itcyborg@outlook.com>
2020-10-18 17:33:06 +0300

    updates

--- database/migrations/2020_05_31_183327_create_loan_applications_table.php
+++ database/migrations/2020_05_31_183327_create_loan_applications_table.php
@@ -15,20 +15,21 @@ class CreateLoanApplicationsTable extends Migration
     {
         Schema::create('loan_applications', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('client_id');
             $table->unsignedBigInteger('product_id');
-            $table->unsignedBigInteger('user_id');
-            $table->enum('status',['PENDING','APPROVED','DISBURSED','REJECTED','SETTLED'])->default('PENDING');
             $table->unsignedDouble('rate');
             $table->unsignedDouble('amount_applied');
+            $table->longText('purpose')->nullable();
             $table->unsignedDouble('amount_approved')->nullable();
             $table->unsignedDouble('total_interest')->default(0);
             $table->unsignedInteger('duration');
             $table->timestamp('due_date')->nullable();
             $table->timestamp('approval_date')->nullable();
             $table->timestamp('disbursement_date')->nullable();
-            $table->longText('purpose')->nullable();
             $table->enum('repayment_frequency',['DAILY','WEEKLY','MONTHLY','QUARTERLY']);
+            $table->unsignedDouble('total_amount_repaid')->default(0);
+            $table->enum('status',['PENDING','APPROVED','DISBURSED','REJECTED','SETTLED'])->default('PENDING');
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
